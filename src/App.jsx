9530b1cd11c5e8429f59371771dd3bec74da0b3b
Preethi <prtuxd@gmail.com>
2025-12-11 20:05:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { ChatInput } from "./components/ChatInput";
+import ChatMessages from "./components/ChatMessages";
+
+import "./App.css";
+
+function App() {
+  //below line - lifting the state up
+  const [chatMessages, setChatMessages] = useState([
+    {
+      message: "hello chatbot",
+      sender: "user",
+      id: "id1",
+    },
+    {
+      message: "Hello! How can I help you?",
+      sender: "robot",
+      id: "id2",
+    },
+    {
+      message: "can you get me todays date",
+      sender: "user",
+      id: "id3",
+    },
+    {
+      message: "Today is October 13",
+      sender: "robot",
+      id: "id4",
+    },
+  ]);
+
+  return (
+    <div className="app-container">
+      <ChatMessages chatMessages={chatMessages} />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  );
+}
+
+export default App;
